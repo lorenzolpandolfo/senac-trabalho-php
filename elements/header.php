525ee03895c0e16c20deb1ab9bbcc4ca61a2f0b6
lorenzolpandolfo <lorenzopandolfo2004@gmail.com>
2024-11-27 10:36:54 -0300

    enviando header

---
+++ elements/header.php
@@ -0,0 +1,26 @@
+<div class="header">
+  <span class="title">Techio</span>
+  <nav>
+    <ul class="header-nav">
+      <li>
+        <a href="/teste">Sobre</a>
+      </li>
+
+      <li>
+        <a href="/teste">Serviços</a>
+      </li>
+
+      <li>
+        <a href="/teste">Projetos</a>
+      </li>
+
+      <li>
+        <a href="/teste">Notícias</a>
+      </li>
+
+      <li>
+        <a href="/teste">Contato</a>
+      </li>
+    </ul>
+  </nav>
+</div>
