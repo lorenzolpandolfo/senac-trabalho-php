e5866ad344d288dd3e3382898cc1ec6cbc06c256
lorenzolpandolfo <lorenzopandolfo2004@gmail.com>
2024-11-29 11:18:36 -0300

    admin login

---
+++ admin/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/css/style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Sora:wght@100..800&display=swap" rel="stylesheet">
+   <title>Admin | login</title>
+</head>
+<body>
+<?php
+    include '../elements/header.php';
+  ?>
+  <main class="center-container">
+    <?php
+      include '../pages/admin_login.php';
+    ?>
+  </main>
+  <?php
+    include '../elements/footer.php';
+  ?>
+</body>
+</html>
---
+++ pages/admin_login.php
@@ -0,0 +1,17 @@
+<?php
+include "constants.php";
+?>
+
+<section class="vertical-page admin-login" id="admin_login">
+    <div class="center">
+        <span class="title">Acessar painel administrativo</span>
+    </div>
+    <form action="" method="post" class="content">
+        <input type="email" name="email" id="email" placeholder="email" require>
+        <input type="password" name="password" id="password" placeholder="senha" require>
+        <button type="submit" class="primary-button big-button">acessar</button>
+
+    </form>
+
+
+</section>
