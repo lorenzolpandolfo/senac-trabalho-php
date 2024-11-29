690682534b9f1ca2ce0cc34f41f65d7f6c5eb7ca
Jad3102 <juliaalvesdorneles2@gmail.com>
2024-11-29 11:53:24 -0300

    enviando paginas

---
+++ pages/contact.php
@@ -0,0 +1,14 @@
+<section class="page contact" id="about">
+    <div class="contact-left">
+        <span class="title">Contato</span>
+        <span class="description">
+           <p>Entre em contato conosco.</p>
+        </span>
+    </div>
+    <form method="post">
+		<input type="email" name="email" placeholder="Seu E-mail" required>
+
+		<input type="text" name="mensagem" placeholder="Deixe uma mensagem" required>
+		<input type="submit" name="enviar" value="enviar">
+	</form>
+</section>
