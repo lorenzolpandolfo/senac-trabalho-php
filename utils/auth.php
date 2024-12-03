ef4308fcc6d7c59a48e0ce9de92ba60b89d2e4c7
lorenzolpandolfo <lorenzopandolfo2004@gmail.com>
2024-12-03 16:08:45 -0300

    logica de login e registro

--- utils/auth.php
+++ utils/auth.php
@@ -6,15 +6,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     print_r($_POST);
     
     $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-    $senha = filter_input(INPUT_POST, 'senha');
+    $password = filter_input(INPUT_POST, 'senha');
 
+    // register_user($pdo, "teste", $email, $password);
+    $user = login_user($pdo, $email, $password);
+    
+    if ($user) {
+        session_start();
+        $_SESSION['user'] = $user;
+        header('Location: dashboard.php');
+        exit;
+    }
+}
+
+function register_user($pdo, $fullName, $email, $password) {
     try {
-        // Inserindo o usuário no banco de dados
         $stmt = $pdo->prepare("INSERT INTO user (full_name, email, password) VALUES (:full_name, :email, :password)");
         $stmt->bindParam(':full_name', $fullName);
         $stmt->bindParam(':email', $email);
-        // Hash da senha para segurança
-        $passwordHash = password_hash($senha, PASSWORD_BCRYPT);
+
+        $passwordHash = password_hash($password, PASSWORD_BCRYPT);
         $stmt->bindParam(':password', $passwordHash);
 
         if ($stmt->execute()) {
@@ -27,6 +38,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 
+function login_user($pdo, $email, $password) {
+    try {
+        $stmt = $pdo->prepare("SELECT * FROM user WHERE email = :email");
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
 
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($user) {
+            if (password_verify($password, $user['password'])) {
+                echo "Login realizado com sucesso!";
+                return $user;
+            } else {
+                echo "Senha incorreta.";
+                return null;
+            }
+        } else {
+            echo "Usuário não encontrado.";
+            return null;
+        }
+    } catch (PDOException $e) {
+        echo "Erro ao executar a operação: " . $e->getMessage();
+        return null;
+    }
+}
 
 ?>
