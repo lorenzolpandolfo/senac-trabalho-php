ceee74bf04e385c816264ac62666feaeed519de1
lorenzolpandolfo <lorenzopandolfo2004@gmail.com>
2024-12-03 21:26:41 -0300

    enviando perfil do usuario com placeholders

--- dashboard/index.php
+++ dashboard/index.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Sora:wght@100..800&display=swap" rel="stylesheet">
@@ -17,9 +17,10 @@
     <?php
         include "elements/header.php";
         ?>
-    <main class="center-container dsb-spacing">
+    <main class="center-container dsb-spacing dashboard">
         <?php
             include "elements/sidebar.php";
+            include "sections/profile.php";
         ?>
     </main>
 </body>
---
+++ dashboard/sections/profile.php
@@ -0,0 +1,24 @@
+<div class="dashboard-section">
+    <span class="title">Meu perfil</span>
+    <div class="dashboard-center">
+        <div class="top-div">
+            <img src="https://i.pinimg.com/236x/68/31/12/68311248ba2f6e0ba94ff6da62eac9f6.jpg" alt="user profile">
+            <input type="button" value="editar" class="btn secondary-button">
+        </div>
+        <div>
+            <div>
+                <input type="button" value="editar" class="btn secondary-button">
+                <span>Lorenzo Pandolfo</span>    
+            </div>
+            <div>
+                <input type="button" value="editar" class="btn btn secondary-button">
+                <span>lorenzopandolfo@gmail.com</span>    
+            </div>
+            <div>
+                <input type="button" value="editar" class="btn secondary-button">
+                <span>Administrador</span>    
+            </div>
+        </div>
+        <button type="submit" class="btn primary-button big-button">confirmar</button>
+    </div>
+</div>
